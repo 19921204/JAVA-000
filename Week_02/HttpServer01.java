c0e3dcbd833935b06f05f3e26443fa0bbbd0ebdf
wudongdong <kevin.650915@gmail.com>
2020-10-28 21:35:02 +0800

    update homework

--- Week_02/HttpServer01.java
+++ Week_02/HttpServer01.java
@@ -21,12 +21,13 @@ public class HttpServer01 {
     private static void service(Socket socket) {
         try {
             Thread.sleep(20);
-            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), false);
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
             printWriter.println("HTTP/1.1 200 OK");
             printWriter.println("Content-Type:text/html;charset=utf-8");
+            String body = "hello,nio";
+            printWriter.println("Content-Length:" + body.getBytes().length);
             printWriter.println();
-            printWriter.write("1");
-            printWriter.flush();
+            printWriter.write(body);
             printWriter.close();
             socket.close();
         } catch (IOException | InterruptedException e) {
--- Week_02/HttpServer02.java
+++ Week_02/HttpServer02.java
@@ -4,7 +4,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HttpServer02 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8802);
         while (true) {
             try {
@@ -17,15 +17,17 @@ public class HttpServer02 {
             }
         }
     }
-    
+
     private static void service(Socket socket) {
         try {
             Thread.sleep(20);
             PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
             printWriter.println("HTTP/1.1 200 OK");
             printWriter.println("Content-Type:text/html;charset=utf-8");
+            String body = "hello,nio";
+            printWriter.println("Content-Length:" + body.getBytes().length);
             printWriter.println();
-            printWriter.write("hello,nio");
+            printWriter.write(body);
             printWriter.close();
             socket.close();
         } catch (IOException | InterruptedException e) {
